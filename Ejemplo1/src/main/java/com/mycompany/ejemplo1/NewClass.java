a0337d67001629d076669fcadeb16dbad30247cb
RonalGSDev <gomezronal986@gmail.com>
2024-08-13 18:32:28 -0600

    Se agrega versionamiento de java

---
+++ Ejemplo1/src/main/java/com/mycompany/ejemplo1/NewClass.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejemplo1;
+
+/**
+ *
+ * @author gomez
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        
+        System.out.println("Hola Mundo");
+        
+    }
+}
